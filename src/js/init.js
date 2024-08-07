59b5c73e5770e3450e6c6161ad9a11819e1ad669
LF <958881587@qq.com>
2024-08-07 14:51:03 +0800

    perf: 封装init方法

--- src/js/init.js
+++ src/js/init.js
@@ -1,66 +1,61 @@
-import * as THREE from '../../three.module.js';
-import { OrbitControls } from '../../examples/jsm/controls/OrbitControls.js';
-import Stats from '../../examples/jsm/libs/stats.module.js';
+import * as THREE from "../../three.module.js";
+import { OrbitControls } from "../../examples/jsm/controls/OrbitControls.js";
+import Stats from "../../examples/jsm/libs/stats.module.js";
 
-export function init(
-	dom,
-	params = {}
-) {
-	const options = {
-		isStat: true,
-		isAxesHelper: true,
-		...params
-	}
-	let {width,height} = getDomWidthAndHeight(dom);
-	let scene, camera, renderer, controls, stat, axesHelper;
-	//scene
-	scene = new THREE.Scene();
-	scene.background = new THREE.Color(0x000000);
-	//camera
-	camera = new THREE.PerspectiveCamera(
-		75,
-		width / height,
-		0.1,
-		1000
-	);
-	camera.position.set(10, 10, 10);
-	camera.lookAt(scene.position);
-	//renderer
-	renderer = new THREE.WebGLRenderer({ antialias: true });
-	renderer.setSize(width, height);
-	renderer.setPixelRatio(dom.devicePixelRatio);
-	renderer.setAnimationLoop(animate);
-	dom.appendChild(renderer.domElement);
-	//orbit controls
-	controls = new OrbitControls(camera, renderer.domElement);
-	//stats
-	if (options.isStat) {
-		stat = new Stats();
-		dom.appendChild(stat.domElement);
-	}
-	//axes
-	if (options.isAxesHelper) {
-		axesHelper = new THREE.AxesHelper(5);
-		scene.add(axesHelper);
-	}
-	//resize
-	window.addEventListener('resize', onWindowResize);
-	function onWindowResize() {
-		let {width,height} = getDomWidthAndHeight(dom);
-		camera.aspect = width / height;
-		camera.updateProjectionMatrix();
-		renderer.setSize(width, height);
-	}
-	function animate() {
-		controls && controls.update();
-		stat && stat.update();
-		renderer.render(scene, camera);
-	}
-	function getDomWidthAndHeight() {
-		return {
-			width: dom.clientWidth||window.innerWidth,
-			height: dom.clientHeight||window.innerHeight
+class Scene {
+	constructor() {}
+	async init(dom, params = {}) {
+		this.options = {
+			isStat: true,
+			isAxesHelper: true,
+			...params,
+		};
+		this.dom = dom;
+		let { width, height } = this.getDomWidthAndHeight();
+		//scene
+		this.scene = new THREE.Scene();
+		this.scene.background = new THREE.Color(0x000000);
+		//camera
+		this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+		this.camera.position.set(10, 10, 10);
+		this.camera.lookAt(this.scene.position);
+		//renderer
+		this.renderer = new THREE.WebGLRenderer({ antialias: true });
+		this.renderer.setSize(width, height);
+		this.renderer.setPixelRatio(dom.devicePixelRatio);
+		this.renderer.setAnimationLoop(this.animate.bind(this));
+		dom.appendChild(this.renderer.domElement);
+		//orbit controls
+		this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+		//stats
+		if (this.options.isStat) {
+			this.stat = new Stats();
+			dom.appendChild(this.stat.domElement);
 		}
+		//axes
+		if (this.options.isAxesHelper) {
+			this.axesHelper = new THREE.AxesHelper(5);
+			this.scene.add(this.axesHelper);
+		}
+		//resize
+		window.addEventListener("resize", this.onWindowResize.bind(this));
 	}
-	return { scene, camera, renderer, controls, stat };
+	animate() {
+		this.controls && this.controls.update();
+		this.stat && this.stat.update();
+		this.renderer.render(this.scene, this.camera);
+	}
+	getDomWidthAndHeight() {
+		return {
+			width: this.dom.clientWidth || window.innerWidth,
+			height: this.dom.clientHeight || window.innerHeight,
+		};
+	}
+	onWindowResize() {
+		let { width, height } = this.getDomWidthAndHeight();
+		this.camera.aspect = width / height;
+		this.camera.updateProjectionMatrix();
+		this.renderer.setSize(width, height);
+	}
 }
+export default new Scene();
